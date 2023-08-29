39528e27f0b14fcc29a15fb0cef4d0a0204871c3
ghada <ghadaemad011@gmail.com>
2023-08-29 23:32:20 +0300

    final

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Ghada-Emad1/Book_Store",
+  base: "/Book_Store",
   plugins: [react()],
 })
