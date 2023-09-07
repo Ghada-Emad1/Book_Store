ea179885a5f5aeca239ff406a9345125fe73df07
ghada <ghadaemad011@gmail.com>
2023-09-07 20:55:20 +0300

    modify

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Book_Store/",
+  base: "/Book_Store/book-store",
   plugins: [react()],
 })
